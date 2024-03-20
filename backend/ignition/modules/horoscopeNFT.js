bb5ecec740ab28e64a4bf81cb29e462090c88b5c
harsh <harsh3j53@gmail.com>
2024-03-20 21:29:45 +0530

    2nd commit

--- backend/ignition/modules/horoscopeNFT.js
+++ backend/ignition/modules/horoscopeNFT.js
@@ -1,11 +1,19 @@
+const { ethers } = require("ethers");
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+const { PRIVATE_KEY } = process.env;
 
-module.exports = buildModule("horoscopeNFT", (m) => {
+module.exports = buildModule("horoscopeNFT", async (m) => {
   const horoscopeNFT = m.contract("horoscopeNFT");
-  const recipient = "your_recepient_address";
-  const zodiacSign = "zodiacSign";
+  const provider = new ethers.providers.JsonRpcProvider("https://polygon-mumbai.g.alchemy.com/v2/d13gG0RvRZ8mHqYMnnSEyBpkRuvUiHHt"); // Replace with your provider URL
+  const wallet = new ethers.Wallet(`0x${PRIVATE_KEY}`, provider); // Replace with your wallet private key
+  const signer = wallet.connect(provider);
 
-  m.call(horoscopeNFT, "mintNFT", [recipient, zodiacSign]);
+  // Deploy the contract
+  const deployTx = await horoscopeNFT.deploy().connect(signer);
+  await deployTx.wait();
 
-  return { horoscopeNFT };
+  // Get the transaction hash
+  console.log("Transaction hash:", deployTx.hash);
+
+  return { horoscopeNFT: deployTx.contract };
 });
